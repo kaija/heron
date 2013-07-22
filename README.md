656899bfa2ca78a5c3abf000f32b5fea88e34a85
Kaija <kaija.chang@gmail.com>
2013-07-22 22:51:34 +0800

    add query_id / query_id_key / insert / delete_id_key / delete_id API

--- README.md
+++ README.md
@@ -3,3 +3,21 @@ Heron
 
 Heron is a fast key / value REST API web service.
 It's based on Redis database and node.js.
+
+Example:
+
+* Insert a id with value 12345
+curl http://localhost:9999/heron/v1/insert -d "id=kaija&val=12345"
+
+* query all timestamp with a id
+curl --dump-header - "http://localhost:9999/heron/v1/query_id?id=kaija"
+
+* query a value of a timestamp
+curl --dump-header - "http://localhost:9999/heron/v1/query_id_key?id=kaija&time=1374503768033"
+
+* delete a timestam value
+curl -i -X DELETE --dump-header - "http://localhost:9999/heron/v1/delete_id_key?id=kaija&time=1374503768033"
+
+* delete all timestamp value with a id
+curl -i -X DELETE --dump-header - "http://localhost:9999/heron/v1/delete_id?id=kaija"
+
--- heron.js
+++ heron.js
@@ -2,39 +2,94 @@ var express = require('express');
 var app = express();
 var redis = require("redis"),
 redis_cli = redis.createClient();
+
 redis_cli.on("error", function (err) {
     console.log("error event - " + redis_cli.host + ":" + redis_cli.port + " - " + err);
 });
 
+redis_conn = false;
+
 redis_cli.on("connect", function(){
     console.log("Connected to local redis server!.");
+    redis_conn = true;
 });
 
+app.use(express.bodyParser());
+
 app.get('/heron/v1/query_id', function(req, res){
-    console.log(req.headers.id);
-    console.log(req);
-    res.send('hello world');
+  if(redis_conn){
+    if(req.query.id != null){
+      console.log("Query with id [" + req.query.id + "]");
+      redis_cli.hkeys(req.query.id, function(err, obj){
+        res.send(200, obj);
+      });
+    }else{
+      res.send(400, "Missing parameter...");
+    }
+  }else{
+    res.send(500, "Database not ready!");
+  }
 });
 
 app.get('/heron/v1/query_id_key', function(req, res){
-    console.log(req);
-    res.send(200);
+  if(redis_conn){
+    if(req.query.id != null && req.query.time != null){
+      console.log("Query with id [" + req.query.id + "]  key [" + req.query.time +"]" );
+      redis_cli.hmget(req.query.id, req.query.time ,function(err, obj){
+        res.send(200, obj);
+      });
+    }else{
+      res.send(400, "Missing parameter...");
+    }
+  }else{
+    res.send(500, "Database not ready!");
+  }
 });
 
 
 app.post('/heron/v1/insert', function(req, res){
-    console.log(req);
-    res.send(200);
+  if(redis_conn){
+    if(req.body.id != null && req.body.val != null){
+      var time = new Date().getTime();
+      console.log("Insert with id [" + req.body.id + "]  time [" + time +"] val ["  + req.body.val + "]");
+      redis_cli.hmset(req.body.id, time, req.body.val);
+      res.send(200);
+    }else{
+      res.send(400, "Missing parameter...");
+    }
+  }else{
+    res.send(500, "Database not ready!");
+  }
 });
 
 app.delete('/heron/v1/delete_id', function(req, res){
-    console.log(req);
-    res.send(200);
+  if(redis_conn){
+    if(req.query.id != null){
+      console.log("Delete id all value [" + req.query.id + "]");
+      redis_cli.del(req.query.id, function(err, obj){
+        res.send(200);
+      });
+    }else{
+      res.send(400, "Missing parameter...");
+    }
+  }else{
+    res.send(500, "Database not ready!");
+  }
 });
 
 app.delete('/heron/v1/delete_id_key', function(req, res){
-    console.log(req);
-    res.send(200);
+  if(redis_conn){
+    if(req.query.id != null && req.query.time != null){
+      console.log("Delete id  [" + req.query.id + "] time [" + req.query.time +"]");
+      redis_cli.hdel(req.query.id, req.query.time, function(err, obj){
+        res.send(200);
+      });
+    }else{
+      res.send(400, "Missing parameter...");
+    }
+  }else{
+    res.send(500, "Database not ready!");
+  }
 });
 
 app.listen(9999);
